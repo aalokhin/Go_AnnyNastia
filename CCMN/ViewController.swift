4fd053ebca29130d71bb8291191ce3e3174c0c0d
aalokhin <n.alokhina@gmail.com>
2019-07-28 17:31:15 +0300

    Initial Commit

---
+++ CCMN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCMN
+//
+//  Created by Anastasiia ALOKHINA on 7/28/19.
+//  Copyright © 2019 Anastasiia ALOKHINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
