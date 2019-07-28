569c806c0dbddb71db75a036a5d583534f20e4e0
aalokhin <n.alokhina@gmail.com>
2019-07-28 17:47:59 +0300

    credentials

--- CCMN/ViewController.swift
+++ CCMN/ViewController.swift
@@ -5,7 +5,20 @@
 //  Created by Anastasiia ALOKHINA on 7/28/19.
 //  Copyright © 2019 Anastasiia ALOKHINA. All rights reserved.
 //
-
+/*
+ 
+ Cisco_CMX_Locate:
+ URL: "https://cisco-cmx.unit.ua"
+ Username: "RO"
+ Password: "just4reading"
+ 
+ Cisco_CMX_Presence:
+ URL: "https://cisco-presence.unit.ua"
+ Username: "RO"
+ Password: "Passw0rd"
+ 
+ 
+ */
 import UIKit
 
 class ViewController: UIViewController {
