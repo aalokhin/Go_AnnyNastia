980e05a37e4245c7d8971b2569fae3665a8c6b96
aalokhin <n.alokhina@gmail.com>
2019-07-31 16:34:35 +0300

    yay

--- CCMN/Controllers/ViewController.swift
+++ CCMN/Controllers/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+    
+    //this shit helps us a  lot
+    var manager : SessionManager = {
+        // Create the server trust policies
+        let serverTrustPolicies: [String: ServerTrustPolicy] = [
+            "cisco-cmx.unit.ua": .disableEvaluation
+        ]
+        
+        // Create custom manager
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
+        
+        let manager = Alamofire.SessionManager(
+            configuration: URLSessionConfiguration.default,
+            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
+        )
+        
+        return manager
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +38,8 @@ class ViewController: UIViewController {
         self.testRequestLocate()
         // Do any additional setup after loading the view.
     }
+    
+    
     
     
     func test(){
@@ -37,46 +59,19 @@ class ViewController: UIViewController {
         
         let urlPath : String = "https://cisco-cmx.unit.ua/api/location/v2/clients/count"
         let url = URL(string: urlPath)
-        
-        let request: NSMutableURLRequest = NSMutableURLRequest(url: url!)
         let auth = "Basic " + String("RO:" + "just4reading").toBase64()
-        request.setValue(auth, forHTTPHeaderField: "Authorization")
-        request.httpMethod = "GET"
-        print(url)
-        DispatchQueue.global().async{
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
-            if let err = error {
-                print("error occured \(err)")
-            } else if let d = data
-            {
-               print("some data received")
-            print(d)
-            }
-        }
-        task.resume()
-    }
         
-    }
-
-
-}
-/*
-func paramJsonRequest(name: String, dopUrl: String, param: [String:Any], completion: @escaping (Bool) -> Void) {
-    let data = requests[name]! //"clientsInfo" : ("https://cisco-cmx.unit.ua/api/location/v2/clients", "just4reading"),
-    let auth = "Basic " + String("RO:" + data.password).toBase64()
-    Manager.request(data.url + dopUrl, method: .get, parameters: param, headers: ["Authorization": auth]).validate().responseJSON { response in
-        switch response.result {
-        case .success:
-            if let value = response.result.value {
-                self.parser.paramParseAll(name: name, timeStamp: MainData.timeStamp, data: value)
-                completion(true)
+        manager.request(urlPath, method: .get, headers: ["Authorization": auth]).validate().responseJSON { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+                    print(value)
+                    print("success")
+                   
+                }
+            case .failure(let error):
+               print("failure")
             }
-        case .failure(let error):
-            self.errors.append(error.localizedDescription + "\n")
-            self.writeToFile()
-            completion(false)
         }
     }
 }
- 
- */
