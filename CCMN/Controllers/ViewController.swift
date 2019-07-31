2bd14fb467f0070f459cf654d250ddcac22588e9
aalokhin <n.alokhina@gmail.com>
2019-07-31 16:36:30 +0300

    yay

--- CCMN/Controllers/ViewController.swift
+++ CCMN/Controllers/ViewController.swift
@@ -38,39 +38,23 @@ class ViewController: UIViewController {
         self.testRequestLocate()
         // Do any additional setup after loading the view.
     }
-    
-    
-    
-    
-    func test(){
-        let path = "https://projects.intra.42.fr/uploads/document/document/312/ligands.txt"
-        Alamofire.request(path).responseJSON { response in
-            guard response.result.isSuccess else {
-                print("Ошибка при запросе данных \(String(describing: response.result.error))")
-                return
-            }
-          //  print(response)
-        }
-    }
-        
+    //IT WORKED FUCKAS!!!!!
     func testRequestLocate(){
       //  let urlPath : String = "https://cisco-cmx.unit.ua/api/location/v2/clients/count"
-        
-        
         let urlPath : String = "https://cisco-cmx.unit.ua/api/location/v2/clients/count"
-        let url = URL(string: urlPath)
         let auth = "Basic " + String("RO:" + "just4reading").toBase64()
         
         manager.request(urlPath, method: .get, headers: ["Authorization": auth]).validate().responseJSON { response in
             switch response.result {
+                
             case .success:
                 if let value = response.result.value {
                     print(value)
                     print("success")
-                   
                 }
             case .failure(let error):
                print("failure")
+               print(error)
             }
         }
     }
