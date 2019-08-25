bee9da661b53d7f185a68aa3161299693def7f65
aalokhin <n.alokhina@gmail.com>
2019-08-25 13:43:00 +0300

    mutable dictionary created

--- CCMN/Controllers/PresenceVis.swift
+++ CCMN/Controllers/PresenceVis.swift
@@ -18,43 +18,89 @@ class PresenceVisualizationVC : UIViewController {
     var HoursYValues : [String:Double]?
     let HoursForDic = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
     let hours = ["12am-01am", "01am-02am", "02am-03am", "03am-04am", "04am-05am",  "05am-06am", "06am-07am", "07am-08am", "08am-09am", "09am-10am", "10am-11am", "11am-12pm", "12pm-01pm", "01pm-02pm", "02pm-03pm", "03pm-04pm", "04pm-05pm", "05pm-06pm", "06pm-07pm", "07pm-08pm", "08pm-09pm", "09pm-10pm", "10pm-11pm", "11pm-12am"]
+    let HoursDwell = ["FIVE_TO_THIRTY_MINUTES", "ONE_TO_FIVE_HOURS", "EIGHT_PLUS_HOURS", "FIVE_TO_EIGHT_HOURS", "THIRTY_TO_SIXTY_MINUTES"]
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidAppear(_ animated: Bool) {
         self.tableView.reloadData()
+        
     }
     override func viewDidLoad() {
         print("hi from vis vc")
+        
         super.viewDidLoad()
+        
         setupVC()
-       // getDwell()
-        getProximityUsers()
+       // getProximityUsers()
+        getDwellTime()
         print("start-", startDate, "end-", endDate)
         
+        
+    }
+    func getDwellTime(){
+        let siteId = Client.sharedInstance.siteID?.aesUidString ?? "1513804707441"
+        NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/dwell/hourly/yesterday?siteId=\(siteId)", params: [:], method: .get, completion: {
+            data, error in
+            if let d = data{
+                //                if let json = try? JSONSerialization.jsonObject(with: d, options: []){
+                //                    print(json)
+                //
+                //                }
+                
+                
+                let decoder = JSONDecoder()
+                guard let t = try? decoder.decode([String : [String : Double]].self, from: d) else {
+                    print("error decoding json")
+                    return
+                }
+                print(t)
+                var setAllDwell : [String:AnyObject] = [:]
+                var setForDwellPeriods : [String:AnyObject] = [:]
+                // var firstSet = [String : [String : Double]]()
+                for one in self.HoursForDic {
+                    if let value = t[one]{
+                       // print(one, ">>>")
+                        for two in self.HoursDwell{
+                            if let value2 = value[two]
+                            {
+                                setForDwellPeriods[two] = value2 as AnyObject?
+                                //print(two, value2)
+                            }
+                        }
+                       // print(setForDwellPeriods)
+                        setAllDwell[one] = setForDwellPeriods as AnyObject?
+                    }
+                }
+                print("-----------------------------------------------------")
+                print(setAllDwell)
+                print("-----------------------------------------------------")
+            }
+            
+        })
     }
     
     func reqWithParams(){
         if let siteId = Client.sharedInstance.siteID?.aesUId {
-            NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/connected/hourly/today?siteId=\(siteId)", params: [:], method: .get, completion: {
+            NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/connected/hourly/yesterday?siteId=\(siteId)", params: [:], method: .get, completion: {
                 data, error in
                 if let d = data{
                     print("connected/hourly/yesterday \n\n")
-
+                    
                     if let json = try? JSONSerialization.jsonObject(with: d, options: []){
-
+                        
                         print(json)
                     }
                 }
             })
         }
-                
+        
     }
-
+    
     func setupVC() {
         tableView.register(UINib(nibName: EmptyChartCell.nibName(), bundle: nil), forCellReuseIdentifier: EmptyChartCell.reuseIdentifier())
         self.tableView.rowHeight = 400.0
-
+        
     }
     
     func getKPI(){
@@ -72,20 +118,18 @@ class PresenceVisualizationVC : UIViewController {
                     return
                 }
                 t.prinAll()
-
+                
             }
             
         })
         
     }
     //visitor
-    func getDwellTime(){
-        
-    }
+    
     
     func getProximityUsers(){
         let siteId = Client.sharedInstance.siteID?.aesUidString ?? "1513804707441"
-        NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/connected/hourly/today?siteId=\(siteId)", params: [:], method: .get, completion: {
+        NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/connected/hourly/yesterday?siteId=\(siteId)", params: [:], method: .get, completion: {
             data, error in
             if let d = data{
                 let decoder = JSONDecoder()
@@ -101,7 +145,7 @@ class PresenceVisualizationVC : UIViewController {
                     }
                 }
                 self.allUsers.append(firstSet)
-                NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/visitor/hourly/today?siteId=\(siteId)", params: [:], method: .get, completion: {
+                NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/visitor/hourly/yesterday?siteId=\(siteId)", params: [:], method: .get, completion: {
                     data, error in
                     if let d = data{
                         let decoder = JSONDecoder()
@@ -117,7 +161,7 @@ class PresenceVisualizationVC : UIViewController {
                             }
                         }
                         self.allUsers.append(secondSet)
-                        NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/passerby/hourly/today?siteId=\(siteId)", params: [:], method: .get, completion: {
+                        NetworkManager.getRequestData(isLocation: false, endpoint: "api/presence/v1/passerby/hourly/yesterday?siteId=\(siteId)", params: [:], method: .get, completion: {
                             data, error in
                             if let d = data{
                                 let decoder = JSONDecoder()
@@ -133,10 +177,7 @@ class PresenceVisualizationVC : UIViewController {
                                     }
                                 }
                                 self.allUsers.append(thirdSet)
-                                
-                                //print(">>>>>>>>>>>>>>>>>>>>>>>>>>>", self.allUsers)
-                               // self.tableView.reloadData()
-                                
+
                             }
                             
                         })
